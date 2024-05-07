b9d532563c5443b5b45351ad8240119a4b3ba94b
Jack Cogdill <hi@jackcogdill.com>
2024-05-06 20:52:21 -0700

    feat: mongodb hello world

---
+++ src/app/actions.tsx
@@ -0,0 +1,38 @@
+"use server";
+
+import clientPromise from "../lib/mongodb";
+
+type ConnectionStatus = {
+  isConnected: boolean;
+};
+
+export async function connect(): Promise<ConnectionStatus> {
+  try {
+    await clientPromise;
+    // `await clientPromise` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+    //
+    // `const client = await clientPromise`
+    // `const db = client.db("myDatabase")`
+    //
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+
+    return {
+      isConnected: true,
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      isConnected: false,
+    };
+  }
+}
+
+export async function getExercises() {
+  //
+}
+
+export async function addExercise() {
+  //
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+import { connect } from "./actions";
+
+export default async function Home() {
+  const { isConnected } = await connect();
+  const statusClass = isConnected ? "bg-green-500" : "bg-red-500";
   return (
     <main className="min-h-screen p-24">
       <input
@@ -6,6 +10,9 @@ export default function Home() {
         placeholder="Exercise"
         className="w-full p-4 rounded border border-neutral-200 bg-neutral-50 outline-none dark:bg-neutral-900 dark:border-neutral-700"
       ></input>
+      <div
+        className={`${statusClass} absolute bottom-10 right-10 rounded-full w-10 h-10`}
+      ></div>
     </main>
   );
 }
