ac1d92d2c1dc104b6e2d364da831a27a722e9999
Jack Cogdill <hi@jackcogdill.com>
2024-05-06 18:28:33 -0700

    feat: base code

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen p-24">
+      <input
+        type="text"
+        placeholder="Exercise"
+        className="w-full p-4 rounded border border-neutral-200 bg-neutral-100 outline-none dark:bg-neutral-900 dark:border-neutral-700"
+      ></input>
+    </main>
+  );
+}
