204bed3cbe400a3bd13f2c0fca841d94f1202f5b
Jack Cogdill <hi@jackcogdill.com>
2024-05-13 20:57:16 -0700

    feat: add auth with firebase

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -1,33 +1,7 @@
 "use server";
 
-import clientPromise from "../lib/mongodb";
-import { ConnectionStatus } from "../lib/types";
-
-export async function connect(): Promise<ConnectionStatus> {
-  try {
-    await clientPromise;
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
-
-    return ConnectionStatus.CONNECTED;
-  } catch (e) {
-    console.error(e);
-    return ConnectionStatus.DISCONNECTED;
-  }
-}
-
 export async function getExercises() {
-  console.log("fetching...");
-  const client = await clientPromise;
-  const exercises = client.db().collection("exercises");
-  const result = await exercises.find().toArray();
-  console.log("result:", result);
+  //
 }
 
 export async function addExercise() {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,77 @@
 "use client";
 
-import { ConnectionStatus } from "../lib/types";
-import { connect, getExercises } from "./actions";
 import { useState, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+  AuthError,
+} from "firebase/auth";
 
-function getStatusClass(status: ConnectionStatus): string {
-  switch (status) {
-    case ConnectionStatus.PENDING:
-      return "bg-gray-500";
-    case ConnectionStatus.CONNECTED:
-      return "bg-green-500";
-    case ConnectionStatus.DISCONNECTED:
-      return "bg-red-500";
+// Firebase
+const app = initializeApp({
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+});
+const auth = getAuth(app);
+
+async function signIn() {
+  try {
+    const result = await signInWithPopup(auth, new GoogleAuthProvider());
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential?.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+  } catch (e) {
+    const error = e as AuthError;
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
   }
 }
 
 export default function Home() {
-  const [status, setStatus] = useState(ConnectionStatus.PENDING);
+  const [isSignedIn, setSignedIn] = useState(false);
+  const [displayName, setDisplayName] = useState("Guest");
 
   useEffect(() => {
-    connect().then(setStatus);
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setSignedIn(!!user);
+      if (user?.displayName) {
+        setDisplayName(user.displayName);
+      }
+    });
+    return () => unsubscribe();
   });
 
+  if (!isSignedIn) {
+    return (
+      <div>
+        <h1>My App</h1>
+        <p>Please sign-in:</p>
+        <a onClick={() => signIn()}>Sign-in</a>
+      </div>
+    );
+  }
+
   return (
-    <main className="min-h-screen p-24">
-      <form action={getExercises}>
-        <input
-          type="text"
-          placeholder="Exercise"
-          className="w-full p-4 rounded border border-neutral-200 bg-neutral-50 outline-none dark:bg-neutral-900 dark:border-neutral-700"
-        ></input>
-      </form>
-      <div
-        className={`${getStatusClass(status)} absolute bottom-10 right-10 rounded-full w-10 h-10`}
-      ></div>
-    </main>
+    <div>
+      <h1>My App</h1>
+      <p>Welcome {displayName}! You are now signed-in!</p>
+      <a onClick={() => auth.signOut()}>Sign-out</a>
+    </div>
   );
 }
