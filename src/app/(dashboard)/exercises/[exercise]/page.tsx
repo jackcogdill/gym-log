286b042ddffd471276df1d068ce4526d3a6191f4
Jack Cogdill <hi@jackcogdill.com>
2024-07-06 17:31:49 -0700

    feat(ui): remove form visual noise

--- src/app/(dashboard)/exercises/[exercise]/page.tsx
+++ src/app/(dashboard)/exercises/[exercise]/page.tsx
@@ -9,40 +9,27 @@ const defaultWeight = 0; // TODO: read from previous log entry
 
 export default function Page({ params }: { params: { exercise: string } }) {
   const [weight, setWeight] = useState(defaultWeight);
-  const [sets, setSets] = useState<Array<number | undefined>>([]);
+  const [sets, setSets] = useState<number[]>([]);
   const [note, setNote] = useState("");
 
-  const createSetInput = (index: number) => {
-    const id = `set-units-${index}`;
-    return (
+  const setInputs = [];
+  for (let i = 0; i < sets.length + 1; ++i) {
+    setInputs.push(
       <InputGroup className="mb-3">
         <Form.Control
           inputMode="numeric"
           pattern="[0-9]*"
           type="text"
-          placeholder="-"
-          aria-describedby={id}
-          onChange={(e) => onChangeSet(index, e.target.value)}
+          onChange={(e) => onChangeSet(i, e.target.value)}
         />
-        <InputGroup.Text id={id}>reps</InputGroup.Text>
-      </InputGroup>
+      </InputGroup>,
     );
-  };
-
-  const setInputs = [];
-  for (let i = 0; i < sets.length; ++i) {
-    setInputs.push(createSetInput(i));
-  }
-  if (sets.length === 0 || sets[sets.length - 1] !== undefined) {
-    setInputs.push(createSetInput(sets.length));
   }
 
   const onChangeSet = (index: number, value: string) => {
-    const n = Number.parseInt(value);
-    const newReps = Number.isNaN(n) ? undefined : n;
     const newSets = [...sets];
-    newSets[index] = newReps;
-    while (newSets.length > 0 && newSets[newSets.length - 1] === undefined) {
+    newSets[index] = Number.parseInt(value);
+    while (newSets.length > 0 && Number.isNaN(newSets[newSets.length - 1])) {
       newSets.pop();
     }
     setSets(newSets);
@@ -58,10 +45,7 @@ export default function Page({ params }: { params: { exercise: string } }) {
     e.preventDefault();
 
     console.log("weight", weight);
-    console.log(
-      "sets",
-      sets.filter((x) => x),
-    );
+    console.log("sets", sets.filter(Number.isInteger));
     console.log("note", note);
   };
 
--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,6 @@
 import Search from "../search";
 
 export default function Page() {
+  // TODO: read from db
   return <Search exercises={["bench", "squat", "row"]} />;
 }
